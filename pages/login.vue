4be6a0fcac77076f19b16d4bc7674118926bcb4a
John Farrar <johnfarrar@sosapps.com>
2023-02-24 17:11:16 -0500

    SB Login Complete

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="prose w-full max-w-2xl h-9">
     <h1>Log in to {{ title }}</h1>
-    <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+    <button @click="login" class="bg-blue-500 text-white font-bold py-2 px-4 rounded">
       Log in with Github
     </button>
   </div>
@@ -9,4 +9,27 @@
 
 <script setup lang="ts">
 const { title } = useCourse();
+const { query } = useRoute();
+const supabase = useSupabaseClient();
+const user = useSupabaseUser();
+
+watchEffect(async () => {
+  if (user.value) {
+    await navigateTo(query.redirectTo as string, {
+      replace: true,
+    });
+  }
+});
+
+const login = async () => {
+  const redirectTo = `${window.location.origin}${query.redirectTo}`;
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: 'github',
+    options: { redirectTo },
+  });
+
+  if (error) {
+    console.error(error);
+  }
+}
 </script>
